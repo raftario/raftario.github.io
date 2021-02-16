a5e98ec4cd22f7c16fc55b41af28c7847db2d013
Raphaël Thériault <self@raftar.io>
2021-02-16 18:35:38 -0500

    Preload fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,20 @@ class D extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/open-sans.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/raleway-500.woff2"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
         <body className="bg-base3 text-base00 font-body text-base leading-relaxed break-words">
           <Main />
           <NextScript />
